acd9e5007649f21944c37c3d26338e590f5b963c
Jose Luis Molina <jldemolina@gmail.com>
2014-05-05 00:00:55 +0100

    Configuration panel finished.

--- src/radikalchess/view/swing/ConfigurationPanel.java
+++ src/radikalchess/view/swing/ConfigurationPanel.java
@@ -48,7 +48,7 @@ public class ConfigurationPanel extends JFrame {
     }
 
     private JComboBox createPlayerASearchComboBox() {
-        JComboBox comboBox = new JComboBox();
+        final JComboBox comboBox = new JComboBox();
         comboBox.addItem("Minimax");
         comboBox.addItem("Alpha-Beta (3)");
         comboBox.addItem("Alpha-Beta (4)");
@@ -57,13 +57,15 @@ public class ConfigurationPanel extends JFrame {
         comboBox.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if (e.equals("Minimax"))
+                if (e.getStateChange() != ItemEvent.SELECTED)
+                    return;
+                if (comboBox.getSelectedItem().equals("Minimax"))
                     game.setWhitePlayerSearch(new MinimaxSearch(game));
-                else if (e.equals("Alpha-Beta (3)"))
+                else if (comboBox.getSelectedItem().equals("Alpha-Beta (3)"))
                     game.setWhitePlayerSearch(new AlphaBetaSearch(game, 3));
-                else if (e.equals("Alpha-Beta (4)"))
+                else if (comboBox.getSelectedItem().equals("Alpha-Beta (4)"))
                     game.setWhitePlayerSearch(new AlphaBetaSearch(game, 4));
-                else if (e.equals("Alpha-Beta (5)"))
+                else if (comboBox.getSelectedItem().equals("Alpha-Beta (5)"))
                     game.setWhitePlayerSearch(new AlphaBetaSearch(game, 5));
                 else
                     game.setWhitePlayerSearch(new AlphaBetaSearch(game, 6));
@@ -74,7 +76,7 @@ public class ConfigurationPanel extends JFrame {
     }
 
     private JComboBox createPlayerBSearchComboBox() {
-        JComboBox comboBox = new JComboBox();
+        final JComboBox comboBox = new JComboBox();
         comboBox.addItem("Minimax");
         comboBox.addItem("Alpha-Beta (3)");
         comboBox.addItem("Alpha-Beta (4)");
@@ -83,13 +85,15 @@ public class ConfigurationPanel extends JFrame {
         comboBox.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if (e.equals("Minimax"))
+                if (e.getStateChange() != ItemEvent.SELECTED)
+                    return;
+                if (comboBox.getSelectedItem().equals("Minimax"))
                     game.setBlackPlayerSearch(new MinimaxSearch(game));
-                else if (e.equals("Alpha-Beta (3)"))
+                else if (comboBox.getSelectedItem().equals("Alpha-Beta (3)"))
                     game.setBlackPlayerSearch(new AlphaBetaSearch(game, 3));
-                else if (e.equals("Alpha-Beta (4)"))
+                else if (comboBox.getSelectedItem().equals("Alpha-Beta (4)"))
                     game.setBlackPlayerSearch(new AlphaBetaSearch(game, 4));
-                else if (e.equals("Alpha-Beta (5)"))
+                else if (comboBox.getSelectedItem().equals("Alpha-Beta (5)"))
                     game.setBlackPlayerSearch(new AlphaBetaSearch(game, 5));
                 else
                     game.setBlackPlayerSearch(new AlphaBetaSearch(game, 6));
@@ -100,16 +104,18 @@ public class ConfigurationPanel extends JFrame {
     }
 
     private JComboBox createPlayerBHeutisticsComboBox() {
-        JComboBox comboBox = new JComboBox();
+        final JComboBox comboBox = new JComboBox();
         comboBox.addItem("Medium Heuristic");
         comboBox.addItem("Shield Heuristic");
         comboBox.addItem("Attack Heuristic");
         comboBox.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if (e.equals("Shield Heuristic"))
+                if (e.getStateChange() != ItemEvent.SELECTED)
+                    return;
+                if (comboBox.getSelectedItem().equals("Shield Heuristic"))
                     game.setBlackPlayerHeuristic(new ShieldHeuristic());
-                else if (e.equals("Medium Heuristic"))
+                else if (comboBox.getSelectedItem().equals("Medium Heuristic"))
                     game.setBlackPlayerHeuristic(new MediumHeuristic());
                 else
                     game.setBlackPlayerHeuristic(new AttackHeuristic());
@@ -120,16 +126,18 @@ public class ConfigurationPanel extends JFrame {
     }
 
     private JComboBox createPlayerAHeutisticsComboBox() {
-        JComboBox comboBox = new JComboBox();
+        final JComboBox comboBox = new JComboBox();
         comboBox.addItem("Medium Heuristic");
         comboBox.addItem("Shield Heuristic");
         comboBox.addItem("Attack Heuristic");
         comboBox.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if (e.equals("Shield Heuristic"))
+                if (e.getStateChange() != ItemEvent.SELECTED)
+                    return;
+                if (comboBox.getSelectedItem().equals("Shield Heuristic"))
                     game.setBlackPlayerHeuristic(new ShieldHeuristic());
-                else if (e.equals("Medium Heuristic"))
+                else if (comboBox.getSelectedItem().equals("Medium Heuristic"))
                     game.setBlackPlayerHeuristic(new MediumHeuristic());
                 else
                     game.setBlackPlayerHeuristic(new AttackHeuristic());
